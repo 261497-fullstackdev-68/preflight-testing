51d85400ed406d1f918dd6a9d0b5ccbb4e294c79
Pooh <tatakornaow@gmail.com>
2025-08-12 18:19:06 +0700

    frontend test

--- cypress/e2e/frontend.cy.ts
+++ cypress/e2e/frontend.cy.ts
@@ -14,7 +14,7 @@ describe('Auth pages', () => {
           expect($input[0].validationMessage)
             .to.eq('Please fill out this field.');
         });
-      cy.wait(1000)
+      //cy.wait(1000)
     });
 
 
@@ -26,7 +26,7 @@ describe('Auth pages', () => {
           expect($input[0].validationMessage)
             .to.eq('Please fill out this field.');
         });
-      cy.wait(1000)
+      //cy.wait(1000)
     });
 
 
@@ -43,21 +43,21 @@ describe('Auth pages', () => {
 
       cy.wait('@loginRequestFail')
       cy.contains('Incorrect username or password').should('be.visible')
-      cy.wait(1000)
+      //cy.wait(1000)
     })
 
     it('สามารถเปิดหน้า Sign Up ได้', () => {
       cy.contains('Sign up').click()
       // ถ้าไม่มี routing URL change ให้ตรวจสอบ element แทน
       cy.contains('Sign Up').should('be.visible')
-      cy.wait(1000)
+      //cy.wait(1000)
     })
   })
 
   context('Sign Up Page', () => {
     beforeEach(() => {
       cy.contains('Sign up').click()
-      cy.wait(1000)
+      //cy.wait(1000)
     })
 
 
@@ -67,7 +67,7 @@ describe('Auth pages', () => {
       cy.get('input[type="password"]').last().type('654321')
       cy.get('button[type="submit"]').click()
       cy.contains('Passwords do not match.').should('be.visible')
-      cy.wait(1000)
+      //cy.wait(1000)
     })
 
     it('แสดง error เมื่อ username ซ้ำ', () => {
@@ -83,7 +83,7 @@ describe('Auth pages', () => {
 
       cy.wait('@signupRequestFail')
       cy.contains('Username already exists.').should('be.visible')
-      cy.wait(1000)
+      //cy.wait(1000)
     })
 
     it('sign up สำเร็จ', () => {
@@ -98,10 +98,129 @@ describe('Auth pages', () => {
       cy.get('input[type="password"]').first().type('123456')
       cy.get('input[type="password"]').last().type('123456')
       cy.get('button[type="submit"]').click().wait('@signupRequestSuccess')
-      cy.wait(1000)
+      //cy.wait(1000)
       cy.get('.swal2-confirm').click();
     })
   })
 
+  context('สร้าง Todo', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('FRONTEND_URL'))
+      cy.get('input[type="text"]').type('Test1')
+      cy.get('input[type="password"]').type('1234')
+      cy.get('button[type="submit"]').click()
+    })
 
+    it('notification', () => {
+      cy.get('[aria-label="Notification"]').should('be.visible')
+      // cy.get('[aria-label="Notification"]').click()
+    })
+
+    it("should display and can select date correctly", () => {
+
+      cy.get('button[aria-label="Add"]').click();
+      cy.contains("Create New Todo").should("be.visible");
+      cy.get('input[placeholder="Task Title"]').type("test 5");
+      cy.get('textarea[placeholder="Description"]').type("test 5");
+
+      //   start date pick
+      cy.get('input[placeholder="Select start date & time"]').click();
+      cy.contains("Time").should("be.visible");
+      cy.get(".react-datepicker__day--014").click();
+      cy.get(".react-datepicker__time-list-item").contains("00:30").click();
+      cy.get('input[placeholder="Select start date & time"]').should(
+        "have.value",
+        "2025-08-14 00:30"
+
+      );
+
+      //   end date pick
+      cy.get('input[placeholder="Select end date & time"]').click();
+      cy.contains("Time").should("be.visible");
+      cy.get(".react-datepicker__day--014").click();
+      cy.get(".react-datepicker__time-list-item").contains("19:30").click();
+      cy.get('input[placeholder="Select end date & time"]').should(
+        "have.value",
+        "2025-08-14 19:30"
+      );
+
+      cy.get('button[type="submit"]').click();
+      cy.contains("test 5").should("be.visible");
+    });
+
+
+
+  })
+
+  context('Share test 5 to user Test2', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('FRONTEND_URL'))
+      cy.get('input[type="text"]').type('Test1')
+      cy.get('input[type="password"]').type('1234')
+      cy.get('button[type="submit"]').click()
+    })
+
+    it('share todo', () => {
+      cy.contains('test 5').first().should('be.visible')
+      cy.contains('test 5').first().click()
+      cy.get('[aria-label="share"]').should('be.visible')
+      cy.get('[aria-label="share"]').click()
+      cy.contains('Test2').click()
+    })
+
+
+
+  })
+
+  context('รับ shareTodo', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('FRONTEND_URL'))
+      cy.get('input[type="text"]').type('Test2')
+      cy.get('input[type="password"]').type('1234')
+      cy.get('button[type="submit"]').click()
+    })
+
+    it('accept shareTodo จาก Test1 ได้', () => {
+      cy.get('[aria-label="Notification"]').should('be.visible')
+      cy.get('[aria-label="Notification"]').click()
+      cy.contains('Notification').should('be.visible')
+      cy.get('[aria-label="close"]').should('be.visible')
+      cy.get('[aria-label="accept"]').should('be.visible')
+      cy.get('[aria-label="decline"]').should('be.visible')
+      cy.contains('test 5').get('[aria-label="accept"]').click()
+      cy.wait(1000)
+      cy.get('[aria-label="close"]').click()
+      cy.contains('test 5').should('be.visible')
+    })
+  })
+
+  context('delete test 5 for user Test1', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('FRONTEND_URL'))
+      cy.get('input[type="text"]').type('Test1')
+      cy.get('input[type="password"]').type('1234')
+      cy.get('button[type="submit"]').click()
+    })
+
+    it('delete todo', () => {
+      cy.contains('test 5').first().should('be.visible')
+      cy.contains('test 5').first().click()
+      cy.contains('delete').should('be.visible')
+      cy.contains('delete').click()
+      cy.contains('test 5').should('not.exist')
+    })
+  })
+
+  context('เช็คว่า test 5 ถูกลบไปแล้ว', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('FRONTEND_URL'))
+      cy.get('input[type="text"]').type('Test2')
+      cy.get('input[type="password"]').type('1234')
+      cy.get('button[type="submit"]').click()
+    })
+
+    it('เช็คว่า test 5 ถูกลบไปแล้ว', () => {
+      cy.contains('test 5').should('not.exist')
+    })
+  })
 })
